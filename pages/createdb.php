0ea1f46d145cb9e5f32a3b4eb861c024f9205f4e
svdome <svdome@yandex.ru>
2022-06-21 19:47:26 +0300

    .

--- pages/createdb.php
+++ pages/createdb.php
@@ -5,16 +5,15 @@ include_once "functions.php";
 $link = connect();
 
 $ct1 = 'create table countries(id int not null auto_increment primary key, 
-                            country varchar(64) unique
-                            ) default charset= "utf8"';
-                                
+                            country varchar(64) unique) default charset="utf8"';
+
+
 $ct2 = 'create table cities(id int not null auto_increment primary key, 
                             city varchar(64), 
-                            countryid int, 
-                            foreing key(countryid) references countries(id) on delete cascade, 
+                            countryid int, foreign key(countryid) references countries(id) on delete cascade, 
                             ucity varchar(128), 
-                            unique index ucity(city, countryid)
-                            ) default charset="utf8"'; 
+                            unique index ucity(city, countryid)) default charset="utf8"';
+
 
 $ct3 = 'create table hotels(id int not null auto_increment primary key,
                             hotel varchar(64),
@@ -22,27 +21,27 @@ $ct3 = 'create table hotels(id int not null auto_increment primary key,
                             countryid int, foreign key(countryid) references countries(id) on delete cascade,
                             stars int,
                             cost int,
-                            info varchar(1024)) 
-                            default charset = "utf8"';
-                                
+                            info varchar(1024)) default charset = "utf8"';
+
+
 
 $ct4 =  'create table images(id int not null auto_increment primary key,
                             imagepath varchar(255),
-                            hotelid int, foreign key(hotelid) references hotels(id) on delete cascade) 
-                            default charset = "utf8"';
-                                
+                            hotelid int, foreign key(hotelid) references hotels(id) on delete cascade) default charset = "utf8"';
+
+
 $ct5 =  'create table roles(id int not null auto_increment primary key,
-                            role varchar(32)) 
-                            default charset = "utf8"';                             
-                               
+                            role varchar(32)) default charset = "utf8"';
+
+
 $ct6 =  'create table users(id int not null auto_increment primary key,
                             login varchar(32) unique,
                             pass varchar(128),
                             email varchar(128),
                             discount int,
-                            roleid id, foreign key(roleid) references roles(id) on delete cascade,
-                            avatar mediumblob)
-                            default charset = "utf8"';
+                            roleid int, foreign key(roleid) references roles(id) on delete cascade,
+                            avatar mediumblob) default charset = "utf8"';
+
 
 mysqli_query($link, $ct1);
 $err = mysqli_errno($link);
--- pages/functions.php
+++ pages/functions.php
@@ -3,10 +3,9 @@
 function connect(
     $host = 'localhost',
     $user = 'root',
-    $pass = '123',
+    $pass = 'root',
     $dbname = 'travels'
-)
-{
+) {
     $link = mysqli_connect($host, $user, $pass) or die('connection error');
     mysqli_select_db($link, $dbname) or die('DB open error');
     mysqli_query($link, "set names 'utf8'");
