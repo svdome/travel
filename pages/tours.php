4ec5a2baf6b5032078c78f4c8a91c9e09380e6cb
Evgeny Baryshnikov <svdome@yandex.ru>
2022-07-11 15:19:04 +0300

    .

--- pages/tours.php
+++ pages/tours.php
@@ -3,8 +3,8 @@
 <?php
 $link = connect();
 echo '<form action="index.php?page=1" method ="post">';
-echo '<select name="countryid" class="col-sm3 col-md-3 col-lg-3">';
-$res = mysqli_query($link, 'SELECT*from countries order by country');
+echo '<select name="countryid" class="col-sm-3 col-md-3 col-lg-3">';
+$res = mysqli_query($link, 'SELECT * from countries order by country');
 echo '<option value="0">Select country... </option>';
 while ($row = mysqli_fetch_array($res, MYSQLI_ASSOC)) {
     echo '<option value="' . $row['id'] . '">' . $row['country'] . '</option>';
@@ -19,8 +19,8 @@ if (isset($_POST['selcountry'])) {
     if ($countryid == 0) {
         exit();
     }
-    $res = mysqli_query($link, "SELECT * from cities where countryid='. $counryid .'  order by city");
-    echo '<select name="cityid" class="col-sm3 col-md-3 col-lg-3">';
+    $res = mysqli_query($link, 'SELECT * from cities where countryid='. $countryid .' order by city');
+    echo '<select name="cityid" class="col-sm-3 col-md-3 col-lg-3">';
     echo '<option value="0">Select city...</option>';
     while ($row = mysqli_fetch_array($res, MYSQLI_ASSOC)) {
         echo '<option value="' . $row['id'] . '">' . $row['city'] . '</option>';
@@ -32,20 +32,16 @@ if (isset($_POST['selcountry'])) {
 echo '</form>';
 //------------------------------------------------------------------------------------------------------------
 if (isset($_POST['selcity'])) {
-    echo '<br>';
     $cityid = $_POST['cityid'];
-    if ($cityid == 0) {
-        exit();
-    }
-    $res = mysqli_query($link, 'SELECT cities.city, hotels.hotel, hotels.stars, countries.country
-    from cities, hotels, countries
-    where hotels.cityid=cities.id and hotels.countryid=counries.id');
-
-
+    $select = 'SELECT cnt.country as "country", ct.city as "city, h.hotel as "hotel", h.cost as "price", h.stars as "stars", h.id as "hotelid"
+    from hotels h, cities ct, countries cnt
+    where h.cityid=ct.id and h.countryid=cnt.id and h.cityid=$cityid';
+    $res = mysqli_query($link, $select);
+    echo '<table width="100%" class="table table-striped tbtours text-center">';
+    echo '<thead style="font-weight: bold;"><td>Hotel</td><td>Country</td><td>City</td><td>Price</td><td>Stars</td><td>Link</td></thead>';
     while ($row = mysqli_fetch_array($res, MYSQLI_ASSOC)) {
-        echo  $row['hotel'] . ' ' . $row['country'] . '' . $row['city'] . ' ' . $row['cost'] . '' . $row['stars'];
+        
     }
-
     mysqli_free_result($res);
 }
 //---------------------------------------------------------------------------------------------------------------
